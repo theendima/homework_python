0174cd823ed59a9211b521cccc7f320c0f180265
Dmitry <theendima@gmail.com>
2025-11-30 23:35:50 +0200

    +1file

---
+++ test_fixtures.py
@@ -0,0 +1,34 @@
+import pytest
+
+@pytest.fixture(scope="session")
+def browser():
+    print("Браузер")
+
+    yield
+
+    print ("Закрываем браузер")
+
+@pytest.fixture
+def login_page(browser):
+    print("Страница ввода логина")
+    pass
+
+
+
+@pytest.fixture
+def user():
+    print("Юзверь")
+    return "username", "password"
+
+
+def test_login2(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
+
+
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
+
---
+++ test_hub.py
@@ -0,0 +1,11 @@
+
+#import math
+#new *
+def test_sum():
+    a = 5
+    b = 10
+    assert a == b
+
+
+
+
